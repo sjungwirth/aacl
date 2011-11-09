d19e38faa85291fee38347d6319aa8ee9d6b8737
mtou <mtou@charougna.com>
2011-11-09 09:34:10 +0100

    Rename AACL main class to be extendable

--- classes/aacl/core.php
+++ classes/aacl/core.php
@@ -2,7 +2,7 @@
 
 /**
  * Another ACL
- * 
+ *
  * @see			http://github.com/banks/aacl
  * @package		AACL
  * @uses		Auth
@@ -11,18 +11,18 @@
  * @copyright	(c) Paul Banks 2010
  * @license		MIT
  */
-class AACL
+abstract class AACL_Core
 {
 	/**
 	 * All rules that apply to the currently logged in user
-	 * 
+	 *
 	 * @var	array	contains Model_AACL_Rule objects
 	 */
 	protected static $_rules;
-	
+
 	/**
 	 * Grant access to $role for resource
-	 * 
+	 *
 	 * @param	string|Model_Role	string role name or Model_Role object [optional]
 	 * @param	string	resource identifier [optional]
 	 * @param	string	action [optional]
@@ -65,11 +65,11 @@ class AACL
          ))->create();
       }
 	}
-	
+
 	/**
 	 * Revoke access to $role for resource
     * CHANGED: now accepts NULL role
-	 * 
+	 *
 	 * @param	string|Model_Role role name or Model_Role object [optional]
 	 * @param	string	resource identifier [optional]
 	 * @param	string	action [optional]
@@ -155,11 +155,11 @@ class AACL
 		else
 			throw new AACL_Exception_401;
 	}
-	
+
 	/**
 	 * Get all rules that apply to user
     * CHANGED
-	 * 
+	 *
 	 * @param 	Model_User|Model_Role|bool 	User, role or everyone
 	 * @param 	bool		[optional] Force reload from DB default FALSE
 	 * @return 	array
@@ -182,7 +182,7 @@ class AACL
          // User is guest
          else
          {
-            self::$_rules = $select_query->where('role','=', null);                  
+            self::$_rules = $select_query->where('role','=', null);
          }
 
          self::$_rules = $select_query
@@ -192,24 +192,24 @@ class AACL
 
       return self::$_rules;
 	}
-	
+
 	protected static $_resources;
-	
+
 	/**
 	 * Returns a list of all valid resource objects based on the filesstem adn
     * FIXED
-	 * 
+	 *
 	 * @param	string|bool	string resource_id [optional] if provided, the info for that specific resource ID is returned,
 	 * 					if TRUE a flat array of just the ids is returned
-	 * @return	array 
+	 * @return	array
 	 */
 	public static function list_resources($resource_id = FALSE)
-	{		
+	{
 		if ( ! isset(self::$_resources))
 		{
 			// Find all classes in the application and modules
 			$classes = self::_list_classes();
-      
+
 			// Loop throuch classes and see if they implement AACL_Resource
 			foreach ($classes as $i => $class_name)
 			{
@@ -222,22 +222,22 @@ class AACL
 					{
 						continue;
 					}
-	
+
 					// Create an instance of the class
 					$resource = $class->getMethod('acl_instance')->invoke($class_name, $class_name);
-					
+
                // Get resource info
 					self::$_resources[$resource->acl_id()] = array(
 						'actions' 		=> $resource->acl_actions(),
 						'conditions'	=> $resource->acl_conditions(),
 					);
-					
+
 				}
-				
+
 				unset($class);
-			}			
+			}
 		}
-		
+
 		if ($resource_id === TRUE)
 		{
 			return array_keys(self::$_resources);
@@ -246,7 +246,7 @@ class AACL
 		{
 			return isset(self::$_resources[$resource_id]) ? self::$_resources[$resource_id] : NULL;
 		}
-		
+
 		return self::$_resources;
 	}
 
@@ -259,7 +259,7 @@ class AACL
 		{
 			// Remove core module paths form search
 			$loaded_modules = Kohana::modules();
-			
+
 			$exclude_modules = array(
                'database',
                'orm',
@@ -293,9 +293,9 @@ class AACL
         'notices'        => MODPATH.'notices',
         //'editor' => MODPATH.'editor',
         'article' => MODPATH.'article',*/
-				
+
 			$paths = Kohana::include_paths();
-         
+
          // Remove known core module paths
 			foreach ($loaded_modules as $module => $path)
 			{
@@ -305,15 +305,15 @@ class AACL
                //	unset($paths[array_search($path.DIRECTORY_SEPARATOR, $paths)]);
                unset($paths[array_search($path, $paths)]);
 				}
-			}	
+			}
 
 			// Remove system path
 			unset($paths[array_search(SYSPATH, $paths)]);
 			$files = Kohana::list_files('classes', $paths);
 		}
-		
+
 		$classes = array();
-		
+
 		foreach ($files as $name => $path)
 		{
 			if (is_array($path))
@@ -324,15 +324,15 @@ class AACL
 			{
 				// Strip 'classes/' off start
 				$name = substr($name, 8);
-				
+
 				// Strip '.php' off end
 				$name = substr($name, 0, 0 - strlen(EXT));
-				
+
 				// Convert to class name
 				$classes[] = str_replace(DIRECTORY_SEPARATOR, '_', $name);
 			}
 		}
-		
+
 		return $classes;
 	}
 
@@ -351,7 +351,7 @@ class AACL
    {
       $role = Jelly::select('role')->where('name','=',$role)->limit(1)->execute();
       $rules = self::_get_rules($role);
-      
+
       foreach( $rules as $rule )
       {
          if( $rule->allows_access_to($resource,$action)
@@ -363,19 +363,19 @@ class AACL
 
       return false;
    }
-	
+
 	/**
 	 * Force static access
-	 * 
-	 * @return	void 
+	 *
+	 * @return	void
 	 */
 	protected function __construct() {}
-	
+
 	/**
 	 * Force static access
-	 * 
-	 * @return	void 
+	 *
+	 * @return	void
 	 */
 	protected function __clone() {}
-	
-} // End  AACL
+
+} // End  AACL_Core
