27ff67670c46dd353dc77c3525c9410d6dd69534
mtou <mtou@charougna.com>
2011-11-09 09:35:03 +0100

    Create main AACL class extender

---
+++ classes/aacl.php
@@ -0,0 +1,16 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+/**
+ * Another ACL
+ *
+ * @see			http://github.com/banks/aacl
+ * @package		AACL
+ * @uses		Auth
+ * @uses		Jelly
+ * @author		Paul Banks
+ * @copyright	(c) Paul Banks 2010
+ * @license		MIT
+ */
+class AACL extends AACL_Core
+{
+}
